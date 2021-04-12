1067e92837a730aada15a016e0b6b9b6c5f0dda3
hamdalla2 <hamdalla2@yahoo.com>
2021-04-12 12:41:09 +0300

    .

--- src/main/java/com/foodstore/controller/all.java
+++ src/main/java/com/foodstore/controller/all.java
@@ -9,6 +9,7 @@ import com.foodstore.model.Restaurant;
 import com.foodstore.repository.StoreRepo;
 import com.foodstore.repository.RestaurantRepo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 
+@CrossOrigin
 @RestController
 public class all {
     
